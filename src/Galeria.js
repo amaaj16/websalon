60204099a896bb619bf91e3e4ac1afa3c170d0f2
Alejandro Aviña Jimenez <aavinaji@usersad.everis.int>
2019-02-18 17:23:18 -0600

    galeria

--- src/Galeria.js
+++ src/Galeria.js
@@ -1,39 +1,64 @@
 import React, { Component, } from 'react';
-import {Container,Col,Image} from 'react-bootstrap';
+import {Container,Image} from 'react-bootstrap';
 import descarga from './assets/descarga.png';
+import Salon from './assets/Salon.jpg'
 import "./Galeria.css";
 class Galeria extends Component{
   constructor(props){
     super(props);
     this.state={
-      items:[],
+      items:[Salon,descarga,Salon,Salon,Salon,Salon,Salon,Salon,Salon],
       viewImg:false,
-      visibility: 'hidden'
+      visibility: 'hidden',
+      idItem: ''
 
     }
-      this.handleClick = this.handleClick.bind(this);
-
-  }
-  handleClick() {
-    this.setState({visibility: 'visible'})
+    this.handleClick = this.handleClick.bind(this);
 
   }
 
+
+  handleClick(id,e) {
+    if (!this.state.viewImg) {
+      this.setState({viewImg:true,visibility: 'visible',idItem:id})
+    }
+    if(this.state.viewImg){
+      this.setState({viewImg:false,visibility: 'hidden'})
+    }
+
+
+
+
+  }
    render(){
+     const handleClick = this.handleClick;
 
+     function ItemsGaleria(props){
+     const itemsImg = props.itemsS;
+     const listImg = itemsImg.map((item,i)=>(
+       <div className="responsive" key={i}>
+         <div className="gallery" key={i}>
+           <button  onClick={handleClick.bind(this, item)} className="btn-img" key={i} >
+             <Image src={item} width="10px" key={i}></Image>
+           </button>
+         </div>
+       </div>
+
+     )
+   )
+   return (<Container>{listImg}</Container>);
+ }
      return (
        <div className="Galeria">
          <div style={this.state} className="contenedor-img">
            <div style={this.state} className="img-view">
-             <Image src={descarga} thumbnail></Image>
-             <div><button</div>
-           </div>
+             <Image src={this.state.idItem}  thumbnail></Image>
+           <div><button onClick={this.handleClick} className="img-close">X</button></div>
          </div>
-       <Container>
-          <button onClick={this.handleClick}>
-              <Image src={descarga} thumbnail></Image>
-          </button>
-       </Container>
+          </div>
+
+              <ItemsGaleria itemsS={this.state.items}/>
+
 
      </div>
    )
--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import {Container,Image,Row,Col  } from 'react-bootstrap';
 import portada from './assets/fuente-jardin2.jpg';
+import "./Home.css";
 class Home extends Component{
 
    render(){
      return (
      <div className="Home">
      <Container>
-        <Row>
-        <Col xs={12} md={12} >
-          <Image src={portada} fluid/>
-          </Col>
-        </Row>
+          <div className="card">
+            <div className="title"><h1>El Quijote</h1></div>
+            <div>El salon Quijote te da la bienvenida a su portalweb.</div>
+          </div>
      </Container> </div>
    )
    }
