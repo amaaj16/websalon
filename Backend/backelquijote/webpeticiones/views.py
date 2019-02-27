1f5cda811ea101cd7b64862f2340ac4f18f8e90d
Alejandro Aviña Jimenez <aavinaji@usersad.everis.int>
2019-02-26 18:33:04 -0600

    galeria peticion

--- Backend/backelquijote/webpeticiones/views.py
+++ Backend/backelquijote/webpeticiones/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpRequest,JsonResponse
 # Create your views here.
+def galeria(request):
+
+    user = {
+	"name":"admin",
+	"pass":"pass"
+	}
+
+    return JsonResponse(user)
