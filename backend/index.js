2938ede2d4ee542d28f4444fc3948d868a65fb4a
Alejandro Aviña Jimenez <aavinaji@usersad.everis.int>
2019-03-01 13:55:22 -0600

    backend

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,33 @@
+'use strict'
 var express = require('express');
+var bodyParser=require('body-parser');
 var app = express();
+const port = process.env.PORT || 3000
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+  console.log('Time:', Date.now());
+  next();
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.use('/user/:id', function (req, res, next) {
+  console.log('Request Type:', req.method);
+  next();
+});
+
+app.get('/user/:id', function (req, res, next) {
+  res.send('USER');
+});
+
+app.get('/', (req, res) =>{
+  res.json(respuesta);
+});
+const respuesta = {
+  name: "ok",
+  title: "page"
+}
+app.listen(port,()=> {
+  console.log(`Example app listening on port ${port}!`);
 });
