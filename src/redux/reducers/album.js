ed7e1b72a4ff1400f6bd67c5e5d1d12fbea87d30
Alejandro Aviña Jimenez <aavinaji@usersad.everis.int>
2019-03-21 17:43:25 -0600

    graphql cliente

--- src/redux/reducers/album.js
+++ src/redux/reducers/album.js
@@ -1,25 +1,19 @@
 import {type as findAlbum} from '../../redux/actions/findAlbum.js';
 import albums from '../../modelo.js'
-
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
 const defaultState= [];
 
-const query ={
-    query:{
-      albums:{
-        to:'',
-        photo:''
-      }
-    },
-  };
+const client = new ApolloClient({
+  uri: "localhost:4000"
+});
+
+
+
 
 function reducer (state = defaultState, {type}){
   switch (type) {
     case findAlbum :
-    fetch('https://us-central1-backend-90c8e.cloudfunctions.net/api/graphql', {
-  method: 'get',
-  body: query
-})
-.then(response => console.log(response.json()))
       return albums;
     default:
       return state;
