0849d830f7973c47b7f9aa257a7443b493c44e92
Alejandro Aviña Jimenez <aavinaji@usersad.everis.int>
2019-03-20 19:25:56 -0600

    fetch graphql

--- src/redux/reducers/album.js
+++ src/redux/reducers/album.js
@@ -3,9 +3,23 @@ import albums from '../../modelo.js'
 
 const defaultState= [];
 
+const query ={
+    query:{
+      albums:{
+        to:'',
+        photo:''
+      }
+    },
+  };
+
 function reducer (state = defaultState, {type}){
   switch (type) {
     case findAlbum :
+    fetch('https://us-central1-backend-90c8e.cloudfunctions.net/api/graphql', {
+  method: 'get',
+  body: query
+})
+.then(response => console.log(response.json()))
       return albums;
     default:
       return state;
