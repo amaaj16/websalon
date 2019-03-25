3c38f3a08c88cb366d6124f95a2bf1b316fa8454
amaaj16 <amaaj16@hotmail.com>
2019-03-25 07:08:35 -0600

    all

--- src/redux/reducers/album.js
+++ src/redux/reducers/album.js
@@ -1,12 +1,6 @@
 import {type as findAlbum} from '../../redux/actions/findAlbum.js';
 import albums from '../../modelo.js'
-import ApolloClient from "apollo-boost";
-import gql from "graphql-tag";
 const defaultState= [];
-
-const client = new ApolloClient({
-  uri: "localhost:4000"
-});
 
 
 
@@ -14,6 +8,18 @@ const client = new ApolloClient({
 function reducer (state = defaultState, {type}){
   switch (type) {
     case findAlbum :
+    fetch('http://localhost:4000/graphql', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    
+  },
+  mode: 'cors',
+  body: JSON.stringify({query: "{ albums{ name} }"})
+})
+  .then(r => r.json())
+  .then(data => console.log('data returned:', data));
       return albums;
     default:
       return state;
